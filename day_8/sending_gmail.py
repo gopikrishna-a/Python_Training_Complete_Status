d21f470cae5b5810b5ea401c5d3c42a021b432a4
gopikrishna01 <a.gopikrishna01@gmail.com>
2017-01-23 11:18:31 +0530

    Update sending_gmail.py

--- day_8/sending_gmail.py
+++ day_8/sending_gmail.py
@@ -1,3 +1,7 @@
+#Created By A.Gopikrishna on 19/01/2017
+#Language --> Python 2.7
+#Sending Mail from Command Line Through Python Code
+
 import smtplib
 import getpass
 import sys
@@ -7,20 +11,23 @@ server =smtplib.SMTP(host,port)
 server.ehlo()
 server.starttls()
 server.ehlo()
-username = raw_input("gmail")
-v1=sys.argv[0]
-v2=sys.argv[1]
-v3=sys.argv[2]
-password=getpass.getpass()
+username = raw_input("Enter Ur Mail Id :")
+v1=sys.argv[1]
+v2=sys.argv[2]
+v3=sys.argv[3]
+password=getpass.getpass("Enter Ur Password :")
 server.login(username,password)
 to =v1
 sub =v3
 
 file =open(v2,'r')
 mes = file.read()
-message = "subject:  %s\n%s"%(sub,text)
+message = "subject:  %s\n%s"%(sub,mes)
 server.sendmail(username,to,message)
 
+#NOTE
+#type below command in command line
+#python running.py a.gopikrishna01@gmail.com git.txt hii
 
 
 
