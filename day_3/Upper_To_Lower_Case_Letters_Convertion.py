59417baa9899fdbf03313b975abf3e8489063004
gopikrishna01 <a.gopikrishna01@gmail.com>
2017-01-24 16:11:12 +0530

    Create Upper_To_Lower_Case_Letters_Convertion.py

---
+++ day_3/Upper_To_Lower_Case_Letters_Convertion.py
@@ -0,0 +1,10 @@
+#Created by A.Gopikrishna on 13/01/2017
+#Language ---> Python 2.7.3
+#Upper to lower and Lower to upper Case Convertion Using In Built Functions
+
+print "===Upper to lower and Lower to upper==="
+
+A = raw_input('type in something lowercase:')
+B = raw_input('type in the same thing caps lock:')
+print "lowercase converted into upper :",A.upper()
+print "upper case converted into lower case :",B.lower()
