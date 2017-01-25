b8677a5a43c22730573605a2c3fd2bb7a18dc285
gopikrishna01 <a.gopikrishna01@gmail.com>
2017-01-25 16:28:55 +0530

    Add files via upload

---
+++ python_MySql/Connecting_to_MySqldb.py
@@ -0,0 +1,18 @@
+#Cnnecting to MySql database
+import MySQLdb
+
+db = MySQLdb.connect("localhost","root","asm123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+
+print ("Database version : %s " % data)
+
+# disconnect from server
+db.close()
---
+++ python_MySql/Creating_Table_In_Database.py
@@ -0,0 +1,34 @@
+#Creating New table and including columns
+
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("localhost","root","asm123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Drop table if it already exist using execute() method.
+cursor.execute("DROP TABLE IF EXISTS ACCESSPOINTS")
+
+
+# Create table as per requirement
+sql = """CREATE TABLE ACCESSPOINTS(
+         MACAddress VARCHAR(255),
+         IPv4Address VARCHAR(255),
+         IPv6Address VARCHAR(255),
+         Name VARCHAR(255),
+         State VARCHAR(255),
+         Tunnel VARCHAR(255),
+         Sec_Mode VARCHAR(255),
+         Mesh_Role VARCHAR(255),
+         PSK VARCHAR(255),
+         Timer VARCHAR(255),
+         HW_Version VARCHAR(255),
+         SW_Version VARCHAR(255),
+         Model VARCHAR(255),
+         Serial_number VARCHAR(255)
+         );"""
+cursor.execute(sql)
+# disconnect from server
+db.close()
