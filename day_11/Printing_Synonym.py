b63152823b1a21fa18a8e081030632d73d75d505
gopikrishna01 <a.gopikrishna01@gmail.com>
2017-01-25 15:01:48 +0530

    Update and rename running.py to Printing_Synonym.py

---
+++ day_11/Printing_Synonym.py
@@ -0,0 +1,18 @@
+#Created By A.Gopikrishna on 25/01/2017
+#Python 2.7.3
+#Printing Synonym For User input word
+
+
+mydict = {'beautiful': 'attractive','funny': 'humorous','happy': 'joyfull'}#adding words into dictionary
+word = raw_input("Enter Ur Word for Synonym (Lower Case Only):")#taking user input word
+
+
+
+if word in mydict:
+	print "Synonyms for "+word+ " are",(mydict[word])#printing the Synonym if word exists
+else:
+	print "Sorry Word not fount"
+	syn  = raw_input("Enter Synonym of Ur Word To add :")
+	mydict[word] = syn #adding new key and newword to Dictonary as it is not in the dictionary 
+	print mydict
+	
