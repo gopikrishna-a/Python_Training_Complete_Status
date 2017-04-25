4f0768c9a46fc20fefad831b2a7b40dc66dc165f
Gopikrishna <a.gopikrishna01@gmail.com>
2017-04-25 18:01:07 +0530

    Update add_two_numbers.py

--- Python_Practice_Progs/add_two_numbers.py
+++ Python_Practice_Progs/add_two_numbers.py
@@ -1,3 +1,4 @@
+#Type-1
 class addition():
     def function(self,a,b):
         self.a=a
@@ -7,3 +8,17 @@ class addition():
 
 add=addition()
 add.function(2,3)
+
+
+#Type-2
+class addition():
+    def function(self,a,b):
+        self.a=a
+        self.b=b
+        c=a+b
+        print c
+
+aa=addition()
+a=int(raw_input("Enter first Number\n"))
+b=int(raw_input("Enter Second Number\n"))
+aa.function(a,b)
